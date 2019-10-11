8c2ff5a1d5ed441feb587a2afeeba30c5ea2af56
Lucas Delobelle <lucas@ld-web.net>
2019-10-11 11:32:58 +0200

    Entité Newsletter

---
+++ src/Entity/Newsletter.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\NewsletterRepository")
+ */
+class Newsletter
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $email;
+
+    /**
+     * @ORM\Column(type="boolean", options={"default" : true})
+     */
+    private $subscribed;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSubscribed(): ?bool
+    {
+        return $this->subscribed;
+    }
+
+    public function setSubscribed(bool $subscribed): self
+    {
+        $this->subscribed = $subscribed;
+
+        return $this;
+    }
+
+    public function getRemoved(): ?bool
+    {
+        return $this->removed;
+    }
+
+    public function setRemoved(?bool $removed): self
+    {
+        $this->removed = $removed;
+
+        return $this;
+    }
+}
