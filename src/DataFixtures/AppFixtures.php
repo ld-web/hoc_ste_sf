2237b9fc6269fe157eb1656d26b674e85dfc3bb3
Lucas Delobelle <lucas@ld-web.net>
2019-10-14 17:00:38 +0200

    Security Bundle : authentification User avec login

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,12 +3,23 @@
 namespace App\DataFixtures;
 
 use App\Entity\Contact;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        // On injecte le service d'encodage de mot de passe
+        // Par constructeur
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
     public function load(ObjectManager $manager)
     {
         $faker = Factory::create('fr_FR');
@@ -23,6 +34,19 @@ class AppFixtures extends Fixture
 
             $manager->persist($contact);
         }
+
+        $user = new User();
+        $user->setLogin('Bob');
+        // On utilise le service injecté à la construction
+        // Pour encoder notre mot de passe et l'affecter directement
+        // à notre champ Password
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user,
+            'bob1234'
+        ));
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($user);
 
         $manager->flush();
     }
