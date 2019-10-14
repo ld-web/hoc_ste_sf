3bb6d31c1cfd111e66158caea0169b09776fc912
Lucas Delobelle <lucas@ld-web.net>
2019-10-14 12:04:49 +0200

    Installation faker bundle + utilisation faker dans les fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,18 +5,21 @@ namespace App\DataFixtures;
 use App\Entity\Contact;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
+        $faker = Factory::create('fr_FR');
+
         for ($i = 0; $i < 30; $i++) {
             $contact = new Contact();
-            $contact->setNom("Curie")
-                ->setPrenom("Marie")
-                ->setEmail("marie@curie.com")
-                ->setObjet("Ma demande numéro $i")
-                ->setMessage("Mon message numéro $i");
+            $contact->setNom($faker->lastName)
+                ->setPrenom($faker->firstName)
+                ->setEmail($faker->email)
+                ->setObjet($faker->sentence(6, true))
+                ->setMessage($faker->paragraph(7, true));
 
             $manager->persist($contact);
         }
